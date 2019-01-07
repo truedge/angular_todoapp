2628bc4ec6e51ac2d4f83cab5841ebb42bdd335b
Jamie Edge <jamie@edgesoftware.tech>
2019-01-06 20:36:47 -0500

    Updating test cases and decoupling content from component

--- src/todolist/todolist.component.spec.ts
+++ src/todolist/todolist.component.spec.ts
@@ -1,27 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
-import { TodoComponent } from './todo.component';
-describe('TodoComponent', () => {
+import { TodoListComponent } from './todolist.component';
+describe('TodoListComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        TodoComponent
+        TodoListComponent
       ],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(TodoComponent);
+    const fixture = TestBed.createComponent(TodoListComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(TodoComponent);
+    const fixture = TestBed.createComponent(TodoListComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('To Do App');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(TodoComponent);
+  /*it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(TodoListComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to The Angular Portal!');
+  }));*/
 });
